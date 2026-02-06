0e90a7246da705cb147230a796b7ea60b5752cbe
Mustafa ONAL <mustafasaitonal@outlook.com>
2026-02-06 15:13:46 +0100

    chore: produce fake error

--- examples/kortyx-nextjs-chat/src/app/page.tsx
+++ examples/kortyx-nextjs-chat/src/app/page.tsx
@@ -4,6 +4,7 @@ import { ChatWindow } from "@/components/features/chat/chat-window";
 import { ChatProvider } from "@/context/chat-context";
 
 export default function Home() {
+  const test: any = "";
   return (
     <ChatProvider>
       <div className="h-screen w-screen flex items-center justify-center">
